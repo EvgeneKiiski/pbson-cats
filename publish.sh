f56a45e7e30642f7a7344be4cf3f3ed3222352b0
Evgenii.Kiiski <ekijski@gmail.com>
2019-03-17 15:31:47 +0300

    fix dir in publish

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 rm -fr repository/*
 sbt clean compile test publish
-./jfrog bt u /Users/evg/work/pbson/repository/ twistedlogic/pbson/pbson-cats/v0.0.1 ru/twistedlogic/pbson-cats_2.12/0.0.1/
+./jfrog bt u /Users/evg/work/pbson-cats/repository/ twistedlogic/pbson/pbson-cats/v0.0.1 ru/twistedlogic/pbson-cats_2.12/0.0.1/
